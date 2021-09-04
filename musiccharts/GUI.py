ccd864f702b6a973f146993821592fad03620e00
DustinScharf <d.scharf.2002@gmail.com>
2021-09-04 22:03:19 +0200

    Better button texts

--- musiccharts/GUI.py
+++ musiccharts/GUI.py
@@ -38,24 +38,24 @@ class GUI(object):
 
         genre_layout = QGridLayout()
 
-        DRUM_AND_BASS_button = GenreButton(genre_layout, 1, "DRUM_AND_BASS", 0, 0)
-        HARD_TECHNO_button = GenreButton(genre_layout, 2, "HARD_TECHNO", 0, 1)
-        ELECTRONICA_button = GenreButton(genre_layout, 3, "ELECTRONICA", 0, 2)
+        DRUM_AND_BASS_button = GenreButton(genre_layout, 1, "Drum and Bass", 0, 0)
+        HARD_TECHNO_button = GenreButton(genre_layout, 2, "Hard Techno", 0, 1)
+        ELECTRONICA_button = GenreButton(genre_layout, 3, "Electronic", 0, 2)
 
-        HOUSE_button = GenreButton(genre_layout, 5, "HOUSE", 1, 0)
-        TECHNO_button = GenreButton(genre_layout, 6, "TECHNO", 1, 1)
-        TRANCE_button = GenreButton(genre_layout, 7, "TRANCE", 1, 2)
-        HARD_DANCE_AND_HARDCODE_button = GenreButton(genre_layout, 8, "HARD_DANCE_AND_HARDCODE", 2, 0)
-        BREAKS_AND_BREAKBEAT_AND_UK_BASS_button = GenreButton(genre_layout, 9, "BREAKS_AND_BREAKBEAT_AND_UK_BASS", 2, 1)
+        HOUSE_button = GenreButton(genre_layout, 5, "House", 1, 0)
+        TECHNO_button = GenreButton(genre_layout, 6, "Techno", 1, 1)
+        TRANCE_button = GenreButton(genre_layout, 7, "Trance", 1, 2)
+        HARD_DANCE_AND_HARDCODE_button = GenreButton(genre_layout, 8, "Hardcore", 2, 0)
+        BREAKS_AND_BREAKBEAT_AND_UK_BASS_button = GenreButton(genre_layout, 9, "Break(beat) + UK Bass", 2, 1)
 
-        TECH_HOUSE_button = GenreButton(genre_layout, 11, "TECH_HOUSE", 2, 2)
-        DEEP_HOUSE_button = GenreButton(genre_layout, 12, "DEEP_HOUSE", 3, 0)
-        PSY_TRANCE_button = GenreButton(genre_layout, 13, "PSY_TRANCE", 3, 1)
-        MINIMAL_AND_DEEP_TECH_button = GenreButton(genre_layout, 14, "MINIMAL_AND_DEEP_TECH", 3, 2)
-        PROGRESSIVE_HOUSE_button = GenreButton(genre_layout, 15, "PROGRESSIVE_HOUSE", 4, 0)
-        DJ_TOOLS_button = GenreButton(genre_layout, 16, "DJ_TOOLS", 4, 1)
+        TECH_HOUSE_button = GenreButton(genre_layout, 11, "Tech House", 2, 2)
+        DEEP_HOUSE_button = GenreButton(genre_layout, 12, "Deep House", 3, 0)
+        PSY_TRANCE_button = GenreButton(genre_layout, 13, "Psy Trance", 3, 1)
+        MINIMAL_AND_DEEP_TECH_button = GenreButton(genre_layout, 14, "Minimal/Deep Tech", 3, 2)
+        PROGRESSIVE_HOUSE_button = GenreButton(genre_layout, 15, "Progressive House", 4, 0)
+        DJ_TOOLS_button = GenreButton(genre_layout, 16, "DJ Tools", 4, 1)
 
-        DUBSTEP_button = GenreButton(genre_layout, 18, "DUBSTEP", 4, 2)
+        DUBSTEP_button = GenreButton(genre_layout, 18, "Dubstep", 4, 2)
 
         layout.addLayout(genre_layout)
 
