69c973522c376db4cfee5cfa14a9687f4cfd0930
DustinScharf <d.scharf.2002@gmail.com>
2021-09-04 16:34:57 +0200

    Added get all charts button

--- musiccharts/GUI.py
+++ musiccharts/GUI.py
@@ -1,9 +1,16 @@
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QMessageBox, QLineEdit, QLabel, QHBoxLayout
 
+from musiccharts.MusicCharts import MusicCharts
+
 
 class GUI(object):
+    def get_all_charts(self):
+        self.music_charts.all_top_100_to_csv()
+
     def __init__(self):
+        self.music_charts = MusicCharts()
+
         app = QApplication([])
         window = QWidget()
         # window.setWindowIcon(QtGui.QIcon("icon.ico"))
@@ -14,6 +21,10 @@ class GUI(object):
         layout = QVBoxLayout()
 
         layout.addWidget(QLabel("Hello"))
+
+        get_all_charts_button = QPushButton("Get all charts")
+        layout.addWidget(get_all_charts_button)
+        get_all_charts_button.clicked.connect(self.get_all_charts)
 
         window.setLayout(layout)
         window.show()
