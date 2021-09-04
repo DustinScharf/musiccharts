94592de72d64622d5c96c5c29b8662e837b17024
DustinScharf <d.scharf.2002@gmail.com>
2021-09-04 16:38:12 +0200

    Added info to gui

--- musiccharts/GUI.py
+++ musiccharts/GUI.py
@@ -20,11 +20,13 @@ class GUI(object):
 
         layout = QVBoxLayout()
 
-        layout.addWidget(QLabel("Hello"))
+        layout.addWidget(QLabel("Press the button to get the top 100 of all genres into CSV files"))
 
-        get_all_charts_button = QPushButton("Get all charts")
+        get_all_charts_button = QPushButton("Get all top lists")
         layout.addWidget(get_all_charts_button)
         get_all_charts_button.clicked.connect(self.get_all_charts)
+
+        layout.addWidget(QLabel("( The CSV files go into the directory <b>HOME/MusicCharts</b> )"))
 
         window.setLayout(layout)
         window.show()
