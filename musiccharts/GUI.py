cdc33aa42745fe96dd4c914759eda5a452790f7d
DustinScharf <d.scharf.2002@gmail.com>
2021-09-04 16:27:04 +0200

    Gui now outputs hello

--- musiccharts/GUI.py
+++ musiccharts/GUI.py
@@ -1,3 +1,20 @@
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QMessageBox, QLineEdit, QLabel, QHBoxLayout
+
+
 class GUI(object):
     def __init__(self):
-        print("Gui...")
+        app = QApplication([])
+        window = QWidget()
+        # window.setWindowIcon(QtGui.QIcon("icon.ico"))
+        window.setWindowTitle("MusicCharts")
+        window.setMinimumWidth(400)
+        window.setMinimumWidth(300)
+
+        layout = QVBoxLayout()
+
+        layout.addWidget(QLabel("Hello"))
+
+        window.setLayout(layout)
+        window.show()
+        app.exec()
