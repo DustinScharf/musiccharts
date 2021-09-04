b7399ddf83086f06016ae4aa4026d2f35b530064
DustinScharf <d.scharf.2002@gmail.com>
2021-09-04 15:51:11 +0200

    Removed useless comments

--- musiccharts/main.py
+++ musiccharts/main.py
@@ -4,10 +4,6 @@ from musiccharts import genres
 from musiccharts.MusicCharts import MusicCharts
 
 if __name__ == '__main__':
-    # print("This is the name of the script: ", sys.argv[0])
-    # print("Number of arguments: ", len(sys.argv))
-    # print("The arguments are: ", str(sys.argv))
-
     if len(sys.argv) != 2 or not sys.argv[1].isnumeric():
         print("This is only a test version.")
         print("Little Error, but here is the fix:")
