723bb776102efd0da5a1103ba24e8f5693bf001d
DustinScharf <d.scharf.2002@gmail.com>
2021-09-06 13:26:24 +0200

    Removed brackets

--- musiccharts/GUI.py
+++ musiccharts/GUI.py
@@ -59,7 +59,7 @@ class GUI(object):
 
         layout.addWidget(QLabel("<hr>"))
 
-        layout.addWidget(QLabel("( The list will result in a CSV file into the directory <b>HOME/MusicCharts</b> )"))
+        layout.addWidget(QLabel("The list will result in a CSV file into the directory <b>HOME/MusicCharts</b>"))
 
         layout.addWidget(QLabel("<hr>"))
 
